881c30539fa22fa0c2ef048b03805ea8fc190d74
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2024-11-26 00:14:40 +0530

    add dark theme for header and fix bug scrolling of product popup

--- js/main.js
+++ js/main.js
@@ -15,7 +15,19 @@ const themeText = document.querySelector('#theme-wrapper > div:first-child');
 const themeImgSpan = document.querySelector('#theme-image > span');
 
 
+const tracker = document.querySelector('#inner-wrapper');//scroll bar tracker of product popup
+
+
 window.addEventListener('load', () => {
+
+    // for initial theme
+    console.log("initial - ",localStorage.getItem("theme"));
+    const initialTheme = localStorage.getItem("theme") || "light";
+    themeText.innerHTML = initialTheme === "light" ? "Dark theme: Off": "Dark theme: On";
+    themeImgSpan.innerHTML = initialTheme === "light"
+        ? `<svg focusable="false" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24"><rect fill="none" height="24" width="24"></rect><path d="M12,7c-2.76,0-5,2.24-5,5s2.24,5,5,5s5-2.24,5-5S14.76,7,12,7L12,7z M2,13l2,0c0.55,0,1-0.45,1-1s-0.45-1-1-1l-2,0 c-0.55,0-1,0.45-1,1S1.45,13,2,13z M20,13l2,0c0.55,0,1-0.45,1-1s-0.45-1-1-1l-2,0c-0.55,0-1,0.45-1,1S19.45,13,20,13z M11,2v2 c0,0.55,0.45,1,1,1s1-0.45,1-1V2c0-0.55-0.45-1-1-1S11,1.45,11,2z M11,20v2c0,0.55,0.45,1,1,1s1-0.45,1-1v-2c0-0.55-0.45-1-1-1 C11.45,19,11,19.45,11,20z M5.99,4.58c-0.39-0.39-1.03-0.39-1.41,0c-0.39,0.39-0.39,1.03,0,1.41l1.06,1.06 c0.39,0.39,1.03,0.39,1.41,0s0.39-1.03,0-1.41L5.99,4.58z M18.36,16.95c-0.39-0.39-1.03-0.39-1.41,0c-0.39,0.39-0.39,1.03,0,1.41 l1.06,1.06c0.39,0.39,1.03,0.39,1.41,0c0.39-0.39,0.39-1.03,0-1.41L18.36,16.95z M19.42,5.99c0.39-0.39,0.39-1.03,0-1.41 c-0.39-0.39-1.03-0.39-1.41,0l-1.06,1.06c-0.39,0.39-0.39,1.03,0,1.41s1.03,0.39,1.41,0L19.42,5.99z M7.05,18.36 c0.39-0.39,0.39-1.03,0-1.41c-0.39-0.39-1.03-0.39-1.41,0l-1.06,1.06c-0.39,0.39-0.39,1.03,0,1.41s1.03,0.39,1.41,0L7.05,18.36z"></path></svg>`
+        : `<svg focusable="false" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24"><rect fill="none" height="24" width="24"></rect><path d="M12,3c-4.97,0-9,4.03-9,9s4.03,9,9,9s9-4.03,9-9c0-0.46-0.04-0.92-0.1-1.36c-0.98,1.37-2.58,2.26-4.4,2.26 c-2.98,0-5.4-2.42-5.4-5.4c0-1.81,0.89-3.42,2.26-4.4C12.92,3.04,12.46,3,12,3L12,3z"></path></svg>`;
+    if (initialTheme === "dark") document.body.classList.add('dark-theme');
 
     // Restore popup state only when navigating back
     const popupState = sessionStorage.getItem('isPopup');
@@ -27,18 +39,11 @@ window.addEventListener('load', () => {
 
     // for google product icon
     if (sessionStorage.getItem('navigatedGoogleProduct') === 'true') {
-        appsLink.style.backgroundColor = "#dedede";
+        appsLink.style.backgroundColor = initialTheme === "light" ? "#dedede" : "#3a3c3e";
         appsLink.style.setProperty("--pseudo-opacity", 1);
     }
 
-    // for initial theme
-    console.log("initial - ",localStorage.getItem("theme"));
-    const initialTheme = localStorage.getItem("theme") || "light";
-    themeText.innerHTML = initialTheme === "light" ? "Dark theme: Off": "Dark theme: On";
-    themeImgSpan.innerHTML = initialTheme === "light"
-        ? `<svg focusable="false" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24"><rect fill="none" height="24" width="24"></rect><path d="M12,7c-2.76,0-5,2.24-5,5s2.24,5,5,5s5-2.24,5-5S14.76,7,12,7L12,7z M2,13l2,0c0.55,0,1-0.45,1-1s-0.45-1-1-1l-2,0 c-0.55,0-1,0.45-1,1S1.45,13,2,13z M20,13l2,0c0.55,0,1-0.45,1-1s-0.45-1-1-1l-2,0c-0.55,0-1,0.45-1,1S19.45,13,20,13z M11,2v2 c0,0.55,0.45,1,1,1s1-0.45,1-1V2c0-0.55-0.45-1-1-1S11,1.45,11,2z M11,20v2c0,0.55,0.45,1,1,1s1-0.45,1-1v-2c0-0.55-0.45-1-1-1 C11.45,19,11,19.45,11,20z M5.99,4.58c-0.39-0.39-1.03-0.39-1.41,0c-0.39,0.39-0.39,1.03,0,1.41l1.06,1.06 c0.39,0.39,1.03,0.39,1.41,0s0.39-1.03,0-1.41L5.99,4.58z M18.36,16.95c-0.39-0.39-1.03-0.39-1.41,0c-0.39,0.39-0.39,1.03,0,1.41 l1.06,1.06c0.39,0.39,1.03,0.39,1.41,0c0.39-0.39,0.39-1.03,0-1.41L18.36,16.95z M19.42,5.99c0.39-0.39,0.39-1.03,0-1.41 c-0.39-0.39-1.03-0.39-1.41,0l-1.06,1.06c-0.39,0.39-0.39,1.03,0,1.41s1.03,0.39,1.41,0L19.42,5.99z M7.05,18.36 c0.39-0.39,0.39-1.03,0-1.41c-0.39-0.39-1.03-0.39-1.41,0l-1.06,1.06c-0.39,0.39-0.39,1.03,0,1.41s1.03,0.39,1.41,0L7.05,18.36z"></path></svg>`
-        : `<svg focusable="false" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24"><rect fill="none" height="24" width="24"></rect><path d="M12,3c-4.97,0-9,4.03-9,9s4.03,9,9,9s9-4.03,9-9c0-0.46-0.04-0.92-0.1-1.36c-0.98,1.37-2.58,2.26-4.4,2.26 c-2.98,0-5.4-2.42-5.4-5.4c0-1.81,0.89-3.42,2.26-4.4C12.92,3.04,12.46,3,12,3L12,3z"></path></svg>`;
-    if (initialTheme === "dark") document.body.classList.add('dark-theme');
+    
 });
 
 /* ----------------------- setting popup ------------------------- */
@@ -110,7 +115,8 @@ productIcon.addEventListener('mouseenter', (event) => {
 function showProductPopup() {
     productPopup.style.visibility = "visible";
     productPopup.style.opacity = "1";
-    appsLink.style.backgroundColor = "#dedede";
+    appsLink.style.backgroundColor = localStorage.getItem("theme") === "light" ? "#dedede" : "#3a3c3e";
+tracker.scrollTop = 0;    // Set the scroll position to the top
 }
 
 function hideProductPopup() {
@@ -129,7 +135,7 @@ document.addEventListener("DOMContentLoaded", () => {
     appsLink.addEventListener("mouseenter", () => {
         if (!isVisibleTooltip) {
             appsLink.style.cursor = "pointer";
-            appsLink.style.backgroundColor = "rgba(60, 64, 67, .08)";
+            appsLink.style.backgroundColor = localStorage.getItem("theme") === "light" ? "rgba(60, 64, 67, .08)" : "#2d2e31";
             appsLink.style.setProperty("--pseudo-opacity", 1); // Show tooltip
             isVisibleTooltip = true;
         } else {
@@ -159,7 +165,7 @@ document.addEventListener("DOMContentLoaded", () => {
         let isHoverOnTooltip = rect.left < mouseX && mouseX < rect.right && mouseY < rect.bottom + tooltipHeight && mouseY > rect.bottom;
 
         if (!isHoverOnTooltip) {
-            appsLink.style.backgroundColor = isVisiblePopup ? "#dedede" : "transparent";
+            appsLink.style.backgroundColor = isVisiblePopup ? (localStorage.getItem("theme") === "light" ? "#dedede" : "#3a3c3e") : "transparent";
             appsLink.style.setProperty("--pseudo-opacity", 0); // Hide tooltip
             appsLink.style.setProperty("--pseudo-pointer-events", "none"); // Disable interactivity
             isVisibleTooltip = false;
