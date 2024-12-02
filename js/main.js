e2d2d55f4cc595cadd4ce27fc701dc81897b97d2
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2024-12-02 17:49:49 +0530

    selection trending and recent search

--- js/main.js
+++ js/main.js
@@ -88,25 +88,15 @@ function hideSearchPopup(){
 //trending and recent search popup
 document.addEventListener('click', (event) => {
     if(btnClearWrapper.contains(event.target))return;
+
     const currentTheme = localStorage.getItem("theme");
 
-    console.log(event.target);
     if (searchInput.contains(event.target) || startDiv.contains(event.target)) {
-        console.log(searchInput.contains(event.target));
-        console.log(startDiv.contains(event.target));
-        if(!isClikedInput){
-        console.log('clicked');
-        showSearchPopup();
-        }
+        if(!isClikedInput)showSearchPopup();
         if(currentTheme === 'dark')changeTheme(true);
+
     }else if(!inputWrapper.contains(event.target) && !searchInput.contains(event.target) && 
                 !startDiv.contains(event.target) && inputWrapper.classList.contains('input-wrapper-active')){
-        
-        console.log(searchInput.contains(event.target));
-        console.log(startDiv.contains(event.target));
-        console.log(inputWrapper.contains(event.target));
-        
-            console.log('out clicked');
             hideSearchPopup();
             if(currentTheme === 'dark')changeTheme(false);
     }
@@ -115,11 +105,9 @@ document.addEventListener('click', (event) => {
 // toggle clear icon appearance
 searchInput.addEventListener('input', () => {
     if (searchInput.value === '') {
-        console.log('Textarea is empty');
         verticalLine.style.display = 'none';
         btnClear.style.display = 'none';
     } else {
-        console.log('Textarea has content:', searchInput.value);
         verticalLine.style.display = 'block';
         btnClear.style.display = 'flex';
     }
@@ -128,7 +116,7 @@ searchInput.addEventListener('input', () => {
 //clear content in search bar
 btnClearWrapper.addEventListener('click', () => {
     if (searchInput.value === '') return;
-    console.log('clear');
+
     searchInput.value = '';
     searchInput.dispatchEvent(new Event('input'));
     showSearchPopup();
@@ -328,20 +316,15 @@ themeSetting.addEventListener('click', () => {
     const currentTheme = localStorage.getItem("theme");
 
     const updatedTheme = currentTheme === "light" ? "dark" : "light";
-    console.log("updatedTheme - ", updatedTheme);
-    // Update localStorage
-    localStorage.setItem("theme", updatedTheme);
-
-    // Update the text
-    themeText.innerHTML = updatedTheme === "light" ? "Dark theme: Off" : "Dark theme: On";
+    localStorage.setItem("theme", updatedTheme);     // Update localStorage
+    themeText.innerHTML = updatedTheme === "light" ? "Dark theme: Off" : "Dark theme: On";  // Update the text
 
     // Update the SVG icon
     themeImgSpan.innerHTML = updatedTheme === "light"
         ? `<svg focusable="false" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24"><rect fill="none" height="24" width="24"></rect><path d="M12,7c-2.76,0-5,2.24-5,5s2.24,5,5,5s5-2.24,5-5S14.76,7,12,7L12,7z M2,13l2,0c0.55,0,1-0.45,1-1s-0.45-1-1-1l-2,0 c-0.55,0-1,0.45-1,1S1.45,13,2,13z M20,13l2,0c0.55,0,1-0.45,1-1s-0.45-1-1-1l-2,0c-0.55,0-1,0.45-1,1S19.45,13,20,13z M11,2v2 c0,0.55,0.45,1,1,1s1-0.45,1-1V2c0-0.55-0.45-1-1-1S11,1.45,11,2z M11,20v2c0,0.55,0.45,1,1,1s1-0.45,1-1v-2c0-0.55-0.45-1-1-1 C11.45,19,11,19.45,11,20z M5.99,4.58c-0.39-0.39-1.03-0.39-1.41,0c-0.39,0.39-0.39,1.03,0,1.41l1.06,1.06 c0.39,0.39,1.03,0.39,1.41,0s0.39-1.03,0-1.41L5.99,4.58z M18.36,16.95c-0.39-0.39-1.03-0.39-1.41,0c-0.39,0.39-0.39,1.03,0,1.41 l1.06,1.06c0.39,0.39,1.03,0.39,1.41,0c0.39-0.39,0.39-1.03,0-1.41L18.36,16.95z M19.42,5.99c0.39-0.39,0.39-1.03,0-1.41 c-0.39-0.39-1.03-0.39-1.41,0l-1.06,1.06c-0.39,0.39-0.39,1.03,0,1.41s1.03,0.39,1.41,0L19.42,5.99z M7.05,18.36 c0.39-0.39,0.39-1.03,0-1.41c-0.39-0.39-1.03-0.39-1.41,0l-1.06,1.06c-0.39,0.39-0.39,1.03,0,1.41s1.03,0.39,1.41,0L7.05,18.36z"></path></svg>`
         : `<svg focusable="false" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24"><rect fill="none" height="24" width="24"></rect><path d="M12,3c-4.97,0-9,4.03-9,9s4.03,9,9,9s9-4.03,9-9c0-0.46-0.04-0.92-0.1-1.36c-0.98,1.37-2.58,2.26-4.4,2.26 c-2.98,0-5.4-2.42-5.4-5.4c0-1.81,0.89-3.42,2.26-4.4C12.92,3.04,12.46,3,12,3L12,3z"></path></svg>`;
 
-    //Update google logo
-    logo.src = updatedTheme === "light" ? "../img/google-logo.png" : "../img/google-logo-white.png";
+    logo.src = updatedTheme === "light" ? "../img/google-logo.png" : "../img/google-logo-white.png";     //Update google logo
 
     hidePopup();
 
